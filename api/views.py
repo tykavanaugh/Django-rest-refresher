405e960ae3afb84990dfa2589921c72fdbe9cb20
tykavanaugh <tykavanaugh@gmail.com>
2022-05-04 16:54:25 -0700

    update

--- api/views.py
+++ api/views.py
@@ -1,7 +1,18 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework import api_view
+
 # Create your views here.
 
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse('API ROOT POINT',safe=False)
+    api_urls = {
+        'List':'/task-list/',
+        'Detail View':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
