c4373cb43ed14f3f2188dca04b96b145dbac06b9
tykavanaugh <tykavanaugh@gmail.com>
2022-05-04 17:18:07 -0700

    working

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
+from dataclasses import field
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Task
+class TaskAdmin(admin.ModelAdmin):
+    fields = ['title','completed']
+
+admin.site.register(Task,TaskAdmin)
